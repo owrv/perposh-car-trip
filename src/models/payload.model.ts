b2068a0ca003dbdccb243db828193766391498d5
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2024-11-27 11:26:43 -0300

    applied decorators to attributes

--- src/models/payload.model.ts
+++ src/models/payload.model.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class Payload {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
   readonly custumer_id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
   readonly origin!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @IsDifferent('origin', { message: 'Destino deve ser diferente de origem' })
   readonly destination!: string;
 }
